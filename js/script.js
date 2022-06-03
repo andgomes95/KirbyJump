ea0149ec9151a283ae8f18647761af835a89ef64
Andre Lucas Nascimento Gomes <andre.lucas.n.gomes@accenture.com>
2022-06-03 11:07:20 -0300

    basic game based in youtube channel https://www.youtube.com/watch?v=r9buAwVBDhA&list=WL&index=30

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const kirby = document.querySelector('.kirby');
+const pipe = document.querySelector('.pipe');
+const jump =() =>{
+    kirby.classList.add('jump');
+    setTimeout(()=>{
+        kirby.classList.remove('jump');
+    },500);
+}
+
+const loopGame = setInterval(()=>{
+    const pipePosition = pipe.offsetLeft;
+    const kirbyPosition = +window.getComputedStyle(kirby).bottom.replace('px','');
+
+    if(pipePosition <= 100 && pipePosition > 0 && kirbyPosition <= 100){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        kirby.style.animation = 'none';
+        kirby.style.bottom = `${kirbyPosition}px`;
+        kirby.src = './images/kirby-lost.webp';
+
+        clearInterval(loopGame);
+    }
+},10);
+
+document.addEventListener('keydown', jump)
